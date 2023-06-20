7b4528903723ae3685ed2fef5402952c38cfb2a4
Muberra Gungoren <118474261+muberra-gungoren@users.noreply.github.com>
2023-06-20 13:02:23 +0300

    Add files via upload

---
+++ UcgenAlan.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class UcgenAlan {
+    public static void UcgenAlan(String[] args) {
+        // Değişkenlerimizi oluşturalım
+        double a, b, c;
+
+
+        // Kullanıcıdan verilerimizi alalım
+        Scanner input = new Scanner(System.in) ;
+        System.out.println("1. kenarı giriniz: ");
+        a = input.nextDouble();
+        System.out.println("2. kenarı giriniz: ");
+        b = input.nextDouble();
+        System.out.println("3. kenarı giriniz: ");
+        c = input.nextDouble();
+
+        double yariCevre = (a + b + c) / 2;
+        double alan = Math.sqrt(yariCevre * (yariCevre - a) * (yariCevre - b) * (yariCevre - c));
+        System.out.println(" üçgenin Alanı: "+ alan);
+
+
+    }
+}
